3ae23e6760f3bc5020acf6358521df35b4dc749b
mitaly-dev <mitalyy.dev@gmail.com>
2023-02-01 07:52:52 -0700

    added a middleware

--- moon-tech-starter-pack/src/pages/Main/Home.js
+++ moon-tech-starter-pack/src/pages/Main/Home.js
@@ -52,18 +52,18 @@ const Home = () => {
     <div className='max-w-7xl gap-14 mx-auto my-10'>
       <div className='mb-10 flex justify-end gap-5'>
         <button
-        onClick={()=>dispatch(toggle_stock())}
+          onClick={()=>dispatch(toggle_stock())}
           className={`border px-3 py-2 rounded-full font-semibold ${stock===true ? activeClass : null} `}
         >
           In Stock
         </button>
         <button
-        onClick={()=>dispatch(toggle_brands('amd'))}
+         onClick={()=>dispatch(toggle_brands('amd'))}
          className={`border px-3 py-2 rounded-full font-semibold ${brands.includes('amd') ? activeClass : null}`}>
           AMD
         </button>
         <button
-        onClick={()=>dispatch(toggle_brands('intel'))}
+         onClick={()=>dispatch(toggle_brands('intel'))}
          className={`border px-3 py-2 rounded-full font-semibold ${brands.includes('intel') ? activeClass : null}`}>
           Intel
         </button>
